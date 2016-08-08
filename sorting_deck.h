c23d80f8a3465c8ab21df982d02c708386f9d2d1
Higor Anjos <higor.araujo.anjos@gmail.com>
2016-08-08 13:12:17 -0300

    QuickSort signature added

--- sorting_deck.h
+++ sorting_deck.h
@@ -6,6 +6,7 @@ void insertion (int vet[], int n);
 void selection (int vet[], int n);
 void heap_sort(int vet[], int n);
 void merge_sort(int vet[], int n);
+void quicksort(int vet[], int n);
 
 /**TEST ASSETS**/
 bool equal(int lhs[], int rhs[], int n);
